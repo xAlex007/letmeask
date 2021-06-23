d8c7baeb834b99d618272d35d38d8f5290c4a950
Alex Afonso <82684257+xAlex007@users.noreply.github.com>
2021-06-23 06:30:22 -0300

    Começo Aula 3

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,17 +5,37 @@ import '../styles/auth.scss'
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleiconImg from '../assets/images/google-icon.svg'
+import { FormEvent, useState } from 'react'
+import { database } from '../services/firebase'
 
 export function Home() {
     const history = useHistory();
     const { user, signInWithGoogle } = useAuth()
+    const [roomCode, setRoomCode] = useState('')
 
     async function handleCreateRoom() {
         if (!user) {
-            await signInWithGoogle();
+            await signInWithGoogle()
         }
 
-        history.push('/rooms/new');
+        history.push('/rooms/new')
+    }
+
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault()
+
+        if (roomCode.trim() === '') {
+            return
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get()
+
+        if (!roomRef.exists()) {
+            alert('Sala não existe.')
+            return
+        }
+
+        history.push(`/rooms/${roomCode}`)
     }
 
     return (
@@ -34,8 +54,8 @@ export function Home() {
                         Crie sua sala com o Google
                     </button>
                     <div className="separator">Ou entre em uma sala</div>
-                    <form>
-                        <input type="text" placeholder="Digite o código da sala" />
+                    <form onSubmit={handleJoinRoom}>
+                        <input type="text" placeholder="Digite o código da sala" onChange={event => setRoomCode(event.target.value)} value={roomCode} />
                         <Button type="submit">Entrar na sala</Button>
                     </form>
                 </div>
---
+++ src/pages/Room.tsx
@@ -0,0 +1,29 @@
+import logoImg from '../assets/images/logo.svg'
+
+export function Room() {
+    return (
+        <div id="page-room">
+            <header>
+                <div className="content">
+                    <img src={logoImg} alt="LetMeAsk" />
+                    <div>Código</div>
+                </div>
+            </header>
+
+            <main className="content">
+                <div className="room-title">
+                    <h1>Sala React</h1>
+                    <span>4 perguntas</span>
+                </div>
+
+                <form>
+                    <textarea placeholder="Faça sua pergunta..." />
+
+                    <div className="form-footer">
+                        <span>Para enviar uma pergunta, <button>faca login.</button></span>
+                    </div>
+                </form>
+            </main>
+        </div>
+    )
+}
